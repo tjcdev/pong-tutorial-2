512bf75e66aea7e7e17085bb4913e334e24e551e
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-11-03 18:35:10 +0000

    initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public Paddle paddle;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Convert screen's pixel coordinate into game's coordinate
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+        Instantiate (ball);
+
+        Paddle paddle1 = Instantiate (paddle) as Paddle;
+        Paddle paddle2 = Instantiate (paddle) as Paddle;
+
+        paddle1.Init(true); // right paddle
+        paddle2.Init(false); // left paddle
+    }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Init(bool isRightPaddle) {
+
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle) {
+            // Place paddle on the right of the screen
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x; // Move a bit to the left
+        } else {
+            // Place paddle on the left of the screen
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x; // Move a bit to the right
+        }
+
+        // Update this paddles position
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
