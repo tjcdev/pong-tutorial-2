82e07f1e96080838787cce273968fd16fe021aa8
tjcdev <TJCARTWRIGHT91@GMAIL.COM>
2019-11-04 01:10:44 +0000

    Game completed

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -4,33 +4,64 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+
+    [SerializeField]
+    float speed;
+
+    float height;
+
+    string input;
+    public bool isRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        height = transform.localScale.y;
+        speed = 5f;
     }
 
     public void Init(bool isRightPaddle) {
 
         Vector2 pos = Vector2.zero;
 
+        isRight = isRightPaddle;
+
         if (isRightPaddle) {
             // Place paddle on the right of the screen
             pos = new Vector2(GameManager.topRight.x, 0);
             pos -= Vector2.right * transform.localScale.x; // Move a bit to the left
+
+            input = "PaddleRight";
         } else {
             // Place paddle on the left of the screen
             pos = new Vector2(GameManager.bottomLeft.x, 0);
             pos += Vector2.right * transform.localScale.x; // Move a bit to the right
+
+            input = "PaddleLeft";
         }
 
         // Update this paddles position
         transform.position = pos;
+
+        transform.name = input;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Now let's move the paddle
+        // GetAxis is a number between -1 and 1 (-1 for down, 1 for up)
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // Restrict Paddle Movement
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0) {
+            move = 0;
+        }
+
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0) {
+            move = 0;
+        }
+
+        transform.Translate(move * Vector2.up);
     }
 }
